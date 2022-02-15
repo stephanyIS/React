584c7403dc1476b6e5a16faeaac00485ed7bf7c8
stephanyIS <stephanypineda16@gmail.com>
2022-02-14 22:35:57 -0600

    Dia 2

---
+++ my-app-intro-js/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+
+//Variables y constantes  //VARIBLES DE SCOPE
+
+//Variables que no se van a renombrar 
+const nombre = 'Stephany';
+let apellido = 'Pineda';
+
+let valorDado = 5;
+
+console.log(nombre, apellido, valorDado);
+console.log(valorDado);
+
+
+if(true){
+    let valorDado = 6;
+    console.log(valorDado);
+}
